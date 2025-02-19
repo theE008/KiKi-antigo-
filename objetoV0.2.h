c4ebeededcce3582c7c06dbdb3bed91ccc461ce5
Thiago Pereira de Oliveira <1494349@sga.pucminas.br>
2025-02-19 14:08:34 -0300

    Desenvolvimento do sistema de gerenciamento de memória

--- objetoV0.2.h
+++ objetoV0.2.h
@@ -39,6 +39,127 @@
  * - função - um dos tipos de argumentos no objeto complexo. Novamente, desnecessário do usuário ter acesso direto.
  */
 
+/////////////////////////////////////////////////////////////////
+// BIBLIOTECAS
 
+#include <stdlib.h>
+#include <stdarg.h> // para argumentos variados
+#include <stdio.h>
+
+/////////////////////////////////////////////////////////////////
+// DEFINIÇÕES DE LOOPS
+
+/**
+ * Para: Usuários
+ * Descrição: Faz um FOR simplificado.
+*/
+#define loop(var,qnts) \
+for (int var = 0, max = qnts, continuar = 1; \
+var < max && continuar; var++)
+
+/**
+ * Para: Usuários
+ * Descrição: Para o LOOP.
+*/
+#define pararLoop continuar = 0;
+
+/////////////////////////////////////////////////////////////////
+// ERRO, DEBUG E FINALIZAÇÃO
+
+////////////////// VARIÁVEIS
+long int memoriaTotal = 0; // total de memoria usada
+     int lixo         = 0; // lixo do programa não limpo
+
+/**
+ * Para: Programador
+ * Descrição: Finaliza o programa com um código.
+*/       
+void finalizar (int val)
+{
+     if (val)
+        printf ("\n\n\tPROGRAMA FINALZADO COM ERRO CODIGO '%d'\n\n", -val); 
+     
+    printf 
+    (
+        "\n\n\t%s\n\t%s '%d' %s\n\n\t'%d' %s\n", 
+        "FIM DO PROGRAMA", "Finalizado com um total de", 
+        lixo, "lixo", memoriaTotal, "memoria usada"
+    );
+
+    printf ("\n");
+
+    exit (val);
+}
+
+/**
+ * Para: Programador
+ * Descrição: Finaliza o programa se um erro for percebido.
+*/   
+void VERIFY_ERROR (int erro, char* arquivo, int linha, const char* funcao)
+{
+    if (erro)
+    {
+        printf 
+        (
+            "\n\n\tERRO NO ARQUIVO: %s\n\tDA FUNCAO: %s\n\tNA LINHA : %d\n\n", 
+            arquivo,
+            funcao,
+            linha
+        );
+
+        finalizar (erro);
+    }
+}
+
+/**
+ * Para: Usuário
+ * Descrição: Uma forma mais enxuta od VERIFY_ERROR, tendo apenas um valor como condição. Se o valor for verdadeiro, a função dispara um erro.
+*/  
+#define verificarErro(erro) VERIFY_ERROR (erro, __FILE__, __LINE__, __func__);
+
+/**
+ * Para: Programador
+ * Descrição: Uma linha de DEBUG que ajuda a identificar erros no código. Fácil de ver após debugar e fácil de limpar.
+*/ 
+#define DEBUGLINE_DEBUGLINE_DEBUGLINE_DEBUGLINE_DEBUGLINE \
+printf ("\n\tlinha %d\n", __LINE__);
+
+/////////////////////////////////////////////////////////////////
+// GESTÃO DE MEMÓRIA
+
+////////////////// VARIÁVEIS
+int nivel = 0; // salva quantas chaves já foram abertas no programa
+
+/**
+ * Para: Usuário
+ * Descrição: Serve de chaves {}, mas se usadas corretamente, irão agir como garbage collector.
+*/   
+#define M {nivel++;
+#define W nivel--;}
+
+/////////////////////////////////////////////////////////////////
+// FUNÇÕES DE RESERVA
+
+/**
+ * Para: Programador
+ * Descrição: Reserva memória com uma segurança. Desnecessário considerando o resto da biblioteca, já que outras funções de criação de dados serão mais apetitosas.
+*/   
+void* mallocar (size_t tam) 
+{
+    void* tmp = malloc (tam); 
+
+    verificarErro (tmp == NULL); 
+    memoriaTotal++; 
+    lixo++; 
+    
+    return tmp;
+}
+
+/**
+ * Para: Programador
+ * Descrição: Um encapsulamento da função mallocar para facilitar o Programador. Novamente, existem sistemas melhores para reserva de memória nesta biblioteca. Todaria, são construídos acima destes dois.
+*/   
+#define reservar(qnts,tipo) \
+(semPtr_##tipo*) mallocar (qnts * sizeof (semPtr_##tipo));
 
 #endif
